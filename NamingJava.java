d5f696d4c6faf864fa6e0ca5492e487ba8ab24f7
Ivan Batallanos <yanaioros@gmail.com>
2021-10-01 22:37:56 -0400

    Ejercicio 4 NamingJava

---
+++ NamingJava.java
@@ -0,0 +1,28 @@
+             //Upper Camel Case
+public class NamingJava {
+    public static void main(String [] args) {
+        //Diferentes metodos de declaracion de variables
+        int celphone = 77774444;
+        int celPhone = 55553333;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Spain";
+        String _backgroundColor = "Green";
+
+        String currency$ = "MXM";
+        String background_color = "Blue";
+
+        //Definir constantes
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+        //Lower Camel Case
+        String fullName = "Yan Ivan Batallanos Thanatos";
+        int sizeInCentimeters = 26;
+
+        
+    }
+}
