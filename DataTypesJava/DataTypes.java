7bd508f8edb2b982fc1def364501c1f5a8fa4ec1
Ivan Batallanos <yanaioros@gmail.com>
2021-10-02 05:30:19 -0400

    Ejercicio 5 DataTypes

---
+++ DataTypesJava/DataTypes.java
@@ -0,0 +1,16 @@
+package DataTypesJava;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //Tipos de datos
+        
+        //Enteros
+        int n = 123456789;
+        long nL = 12345678901L;
+        
+        //Punto Flotante
+        double nD =  123.456123456;
+        float nF = 123.456F;
+    }
+    
+}
