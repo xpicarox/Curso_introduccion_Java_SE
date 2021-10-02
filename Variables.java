87324eb171ba48235e98a4d146a752620fa168b3
Ivan Batallanos <yanaioros@gmail.com>
2021-10-01 20:43:12 -0400

    Ejercicio 2 variables

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declarando una variable
+        int speed;
+        //Asugnamos un valor a una variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Ivan Batallanos";
+        System.out.println(employeeName);
+    }
+}
