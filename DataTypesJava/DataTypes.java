e23e6236c466b394a0387fbf09c305bca23c71b4
Ivan Batallanos <yanaioros@gmail.com>
2021-10-02 05:55:30 -0400

    Ejercicio 5.1 DataTypes:Inferencia de datos,declando var como tipos de datos

--- DataTypesJava/DataTypes.java
+++ DataTypesJava/DataTypes.java
@@ -11,6 +11,21 @@ public class DataTypes {
         //Punto Flotante
         double nD =  123.456123456;
         float nF = 123.456F;
+
+        //Inferencia de datos: utilizacion declando var como tido de dato
+        var salary = 1000;//int
+            //pension 3%
+        var pension = salary*0.03;//double
+        var totalSalary = salary - pension;
+
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeename = "Ivan Batallanos";
+        System.out.println("EMPLOYEE: " + employeename + " SALARY: " + totalSalary);
+        
+
     }
     
 }
