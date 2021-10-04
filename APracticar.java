eba0b7655fe1c7d34b737bcddf27cfbe12812638
Ivan Batallanos <yanaioros@gmail.com>
2021-10-04 12:19:03 -0400

    A practicar todo lo avansado hasta ahora

---
+++ APracticar.java
@@ -0,0 +1,13 @@
+public class APracticar {
+    public static void main(String [] args) {
+        //Variables
+        //presentacion de mis familiares en variables
+        int ivanBatallanos = 24;
+        String jhonyBebeto = "hermano";
+        String madre = "maritzel";
+
+        //Casting
+        //Tipos de casting implicito y explicito
+        
+    }
+}
