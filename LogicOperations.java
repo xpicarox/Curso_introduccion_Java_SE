607c35b63330f6732eff8be62caec4106116cf33
Ivan Batallanos <yanaioros@gmail.com>
2021-10-09 11:47:44 -0400

    Ejercicio 10 LogicOperations

---
+++ LogicOperations.java
@@ -0,0 +1,30 @@
+public class LogicOperations {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+
+        //Operadores asignacion
+        System.out.println("a es igual a b? -> "+ (a==b));
+        System.out.println("a es diferente a b? -> "+ (a != b));
+
+        //Operadores relaconales
+        System.out.println("a es mayor a b? -> "+ (a > b));
+        System.out.println("a es menor a b? -> "+ (a < b));
+        System.out.println("a es mayor o igual a b? -> "+ (a >= b));
+        System.out.println("a es menor o igual a b? -> "+ (a <= b));
+
+        if(a == b){
+            System.out.println("a es igual a b");
+        }else if(a != b && (a > b)){
+            System.out.println("a es diferente a b");
+        }else if(a > b){
+            System.out.println("a es mayor a b");
+        }else if(a < b){
+            System.out.println("a es menor a b");
+        }else if(a >= b){
+            System.out.println("a es mayor o igual a b");
+        }else if(a <= b){
+            System.out.println("a es menor o igual a b");
+        }
+    }
+}
